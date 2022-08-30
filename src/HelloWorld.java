518e623f94669ed3e1e6e9eb7b2e4841d581ace4
slock19 <slock19@stlawu.edu>
2022-08-30 14:56:18 -0400

    updated file to include today's changes

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,10 @@ public class HelloWorld {
 
     public static void main(String [] args) {
         System.out.println("Hello World");
+
+        int x = 5;
+        boolean z = true;
+        double pi = 3.14159;
     }
 
 }
