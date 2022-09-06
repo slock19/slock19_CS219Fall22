58eff510e8e69b80987e731577f8262386304289
slock19 <slock19@stlawu.edu>
2022-09-06 15:53:50 -0400

    adding new investment calculator file

---
+++ src/InvestmentCalculator.java
@@ -0,0 +1,31 @@
+import Utility.Investment; // Using the investment file from Utility
+
+import java.util.Scanner;
+
+public class InvestmentCalculator {
+
+    public static void main(String [] args) {
+
+        // prompt the user for c, r, t, n
+        // and call the function with the inputted values
+
+        // scanner allows for user input
+        Scanner kbd = new Scanner(System.in);
+
+        System.out.print("Enter the initial investment amount: ");
+        double amount = kbd.nextDouble();
+
+        System.out.print("Enter the interest rate as a decimal: ");
+        double interest = kbd.nextDouble();
+
+        System.out.print("Enter the duration of your investment: ");
+        int time = kbd.nextInt();
+
+        System.out.print("Enter the amount of times the interest will compound  yearly: ");
+        double compound = kbd.nextDouble();
+// calling investment function from investment file
+        System.out.printf("Your investment is worth %.2f", Investment.investment(amount, interest, time, compound));
+
+    }
+
+}
--- src/Utility/Investment.java
+++ src/Utility/Investment.java
@@ -8,8 +8,6 @@ public class Investment {
     }
 
     public static void main(String [] args){
-
-
         System.out.println(investment(1.0,1.0, 1, 1e9));
         System.out.println(investment(1000.0,0.03, 10, 365));
     }
