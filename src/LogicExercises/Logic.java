5b638f1ddd3ae824d5e8a803d920df17a3fb0586
slock19 <slock19@stlawu.edu>
2022-09-13 15:42:08 -0400

    9/13 class

---
+++ src/LogicExercises/Logic.java
@@ -0,0 +1,90 @@
+package LogicExercises;
+
+public class Logic {
+    public static int caughtSpeeding(int speed, boolean isBirthday){
+        if (isBirthday){
+            if (speed <= 65){
+                return 0;
+            }
+            else if (speed < 86) {
+                return 1;
+            }
+            else{
+                return 2;
+            }
+
+        }
+        else {
+            if (speed <= 60){
+                return 0;
+            }
+            else if (speed < 81) {
+                return 1;
+            }
+            else{
+                return 2;
+            }
+        }
+    }
+
+
+    public static boolean cigarParty(int cigars, boolean isWeekend){
+        if (isWeekend) {
+            if (cigars < 40) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+        else {
+            if (cigars < 40){
+                return false;
+            }
+            else if (cigars <= 60) {
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+    }
+
+    public static boolean ordered(int d1, int d2, int d3){
+        if (d1 < d2){
+            if (d3 > d2) {
+                return true;
+            }
+            return false;
+        }
+        else if (d1 > d2){
+            if (d2 > d3){
+                return true;
+            }
+            return false;
+        }
+        else {
+            return false;
+        }
+    }
+
+
+    public static void main(String [] args){
+
+        System.out.println(caughtSpeeding(60, false) == 0);
+        System.out.println(caughtSpeeding(65, false) == 1);
+        System.out.println(caughtSpeeding(65, true) == 0);
+        System.out.println(caughtSpeeding(81, false) == 2);
+        System.out.println(caughtSpeeding(81, true) == 1);
+        System.out.println();
+        System.out.println(cigarParty(30, false) == false);
+        System.out.println(cigarParty(50, false) == true);
+        System.out.println(cigarParty(70, true) == true);
+        System.out.println();
+        System.out.println(ordered(30,40,50) == true);
+        System.out.println(ordered(50,30,10) == true);
+        System.out.println(ordered(30,40,20) == false);
+
+    }
+
+
+}
